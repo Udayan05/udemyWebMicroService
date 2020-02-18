794bcc3131d2791cdef8ef7438635966c95fc369
udayan05 <talk2udayan@gmail.com>
2020-02-19 00:56:43 +0530

    adding microsevice tutorials

---
+++ limits-service/src/main/java/com/demo/microservices/limitsservice/bean/LimitsConfiguration.java
@@ -0,0 +1,26 @@
+package com.demo.microservices.limitsservice.bean;
+
+public class LimitsConfiguration {
+
+	private int maximum;
+	private int minimum;
+
+	public LimitsConfiguration() {
+		super();
+	}
+
+	public LimitsConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+}
