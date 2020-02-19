db40f16f6b2d49e756a35f5ec37daab54ab4be9f
udayan05 <talk2udayan@gmail.com>
2020-02-20 00:41:06 +0530

    currency exchange service created

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'INR', 65, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'EUR', 'INR', 80, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10003, 'CAD', 'INR', 60, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10004, 'AUD', 'INR', 55, 0);
